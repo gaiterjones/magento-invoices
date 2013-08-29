b8dec64a5e85230a0df621c1d5907e97bc8c2062
gaiterjones <paj@gaiterjones.com>
2013-08-29 14:55:23 +0200

    Initial release

--- index.php
+++ index.php
@@ -1,28 +1,18 @@
 <?php
 /**
- *  TEMPLATE FOR PHP APPLICATION
  *  
+ *  Copyright (C) 2013
  *
- *  Copyright (C) 2012 paj@gaiterjones.com
  *
- *	This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *  @category   PAJ
- *  @package    
- *  @license    http://www.gnu.org/licenses/ GNU General Public License
+ *  @who	   	PAJ
+ *  @info   	paj@gaiterjones.com
+ *  @license    blog.gaiterjones.com
  * 	
  *
+ */
+
+ /**
+ * An application to display and print Magento Invoices.
  */
 
 include './config/applicationIncludes.php';
